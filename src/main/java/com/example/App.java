202e3c7231cc01bab47f5a02ebecdd66fa7b0727
devanshawasthi01 <devansh.awasthi01@nagarro.com>
2026-04-17 09:46:12 +0530

    Add subtract and isEven methods to App class

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,16 @@
+package com.example;
+
 public class App {
+
     public int add(int a, int b) {
         return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public boolean isEven(int number) {
+        return number % 2 == 0;
     }
 }
